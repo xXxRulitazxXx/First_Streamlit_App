0366d0329dd08929df8a602cc90d929379ce1dc9
xXxRulitazxXx <77294420+xXxRulitazxXx@users.noreply.github.com>
2023-11-01 16:24:10 -0600

    Update Streamlit_App.py
    Se adecúa para Menú de Restaurante.

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,5 +1,9 @@
 import streamlit
 
-streamlit.title('Ejemplo de Título')
-streamlit.header('Ejemplo de Subtítulo')
-streamlit.text('Ejemplo de Cuerpo de Página')
+streamlit.title('Restaurante Rulitaz')
+streamlit.header('Menú de desayuno')
+streamlit.text('🥣 Omega 3 y avena con arándanos')
+streamlit.text('🥗 Batido de col rizada, espinacas y rúcula')
+streamlit.text('🐔 Huevo duro de gallinas camperas')
+
+streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇')
