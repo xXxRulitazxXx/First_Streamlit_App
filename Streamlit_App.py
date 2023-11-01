a80709dd485d17005114316c5608958d76570f45
xXxRulitazxXx <77294420+xXxRulitazxXx@users.noreply.github.com>
2023-11-01 16:28:37 -0600

    Update Streamlit_App.py
    Se agrega biblioteca "pandas" y se ocupa para agregar catálogo de frutas.

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+--
+Lista_Frutas = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+--
 
 streamlit.title('Restaurante Rulitaz')
 streamlit.header('Menú de desayuno')
@@ -7,3 +11,5 @@ streamlit.text('🥗 Batido de col rizada, espinacas y rúcula')
 streamlit.text('🐔 Huevo duro de gallinas camperas')
 
 streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇')
+streamlit.dataframe(Lista_Frutas)
+
